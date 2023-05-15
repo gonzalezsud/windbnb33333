efae8548845376d5cc49c7a90ae1d23103cf12ca
gonzalezsud <99145218+gonzalezsud@users.noreply.github.com>
2023-05-15 15:24:53 -0600

    Initial commit

---
+++ src/components/Description/Description.js
@@ -0,0 +1,53 @@
+import {
+  Descriptions,
+  SuperHost,
+  TypeHost,
+  Ratings,
+  Title,
+  Wrapper,
+  Images,
+  H1,
+  HeadWrap
+} from "./DescriptionElements";
+import { AiFillStar } from "react-icons/ai";
+import StayData from "../../Data/Data.json";
+
+const Description = () => {
+  return (
+    <>
+      <div>
+        <HeadWrap>
+          <H1>Stays in Finland</H1>
+          {StayData.length !== 0 && <div>{StayData.length}+ Stays</div>}
+        </HeadWrap>
+
+        <Descriptions>
+          {StayData.map((data, index) => {
+            return (
+              <div key={index}>
+                <Images src={data.photo} alt={data.type} />
+                <Wrapper>
+                  {data.superHost === true ? (
+                    <SuperHost>SUPER HOST</SuperHost>
+                  ) : null}
+
+                  <TypeHost>
+                    {data.type}. {data.beds} Beds
+                  </TypeHost>
+
+                  <Ratings>
+                    <AiFillStar style={{ color: "red" }} />
+                    {data.rating}
+                  </Ratings>
+                </Wrapper>
+                <Title>{data.title}</Title>
+              </div>
+            );
+          })}
+        </Descriptions>
+      </div>
+    </>
+  );
+};
+
+export default Description;
---
+++ src/context/AppState.js
@@ -0,0 +1,39 @@
+import AppContext from "./AppContext";
+
+import { useState } from "react";
+import StayData from "../Data/Data";
+const AppState = (props) => {
+  const [location, setLocation] = useState("Location");
+  const [count, setCount] = useState(0);
+  const [navExpand, setNavExpand] = useState(false);
+  const [adult, setAdult] = useState(0);
+  const [child, setChild] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  let filterData = StayData.filter((item) => {
+    return item.city;
+  });
+  return (
+    <AppContext.Provider
+      value={{
+        StayData,
+        filterData,
+        location,
+        setLocation,
+        count,
+        setCount,
+        navExpand,
+        setNavExpand,
+        adult,
+        setAdult,
+        child,
+        setChild,
+        isOpen,
+        setIsOpen
+      }}
+    >
+      {props.children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppState;
